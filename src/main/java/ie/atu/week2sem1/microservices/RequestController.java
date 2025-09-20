cc60ad285a2764b016a697882ba20d25d88bb94c
Bartlomiej Drapinski <g00428194@atu.ie>
2025-09-20 18:28:57 +0100

    Added greetByName

--- src/main/java/ie/atu/week2sem1/microservices/RequestController.java
+++ src/main/java/ie/atu/week2sem1/microservices/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.week2sem1.microservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,12 @@ public class RequestController {
     public String hello()
     {
         return "Hello";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name)
+    {
+        return "Hello " + name + "!";
     }
 
 }
