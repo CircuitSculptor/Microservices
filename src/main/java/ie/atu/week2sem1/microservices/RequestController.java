664905bd001b6bd34ecd948c435392dcec05f09d
Bartlomiej Drapinski <g00428194@atu.ie>
2025-09-20 18:18:14 +0100

    Updated Pom and added new class RequestController

---
+++ src/main/java/ie/atu/week2sem1/microservices/RequestController.java
@@ -0,0 +1,17 @@
+package ie.atu.week2sem1.microservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class RequestController {
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello";
+    }
+
+}
