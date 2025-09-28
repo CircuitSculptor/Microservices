88406e246e4472aa9bb4ee51cf30c35cb6eb71f1
Bartlomiej Drapinski <g00428194@atu.ie>
2025-09-28 15:23:14 +0100

    Commented code

--- src/main/java/ie/atu/week2sem1/microservices/Calculator.java
+++ src/main/java/ie/atu/week2sem1/microservices/Calculator.java
@@ -1,14 +1,13 @@
 package ie.atu.week2sem1.microservices;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping
 public class Calculator {
 
     @GetMapping("/calculate")
+    // Collect 3 pieces of data from the user via the swagger UI
     public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation)
     {
         int result = 0;
@@ -19,12 +18,14 @@ public class Calculator {
         } else if (operation.equals("multiply")){
             result = num1 * num2;
         } else if (operation.equals("divide")){
+            // Check if num2 is 0 and print an error message to the user
             if (num2 == 0){
                 return "Error: Cannot divide by zero";
             } else {
                 result = num1 / num2;
             }
         }
+        // Print results to the user via swagger HTTP
         return String.valueOf(result);
     }
 }
