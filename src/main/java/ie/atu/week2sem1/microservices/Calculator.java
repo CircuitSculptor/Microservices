2f0f33a34cb0eddb022b2fccc682b957c56646e9
Bartlomiej Drapinski <g00428194@atu.ie>
2025-09-28 14:56:42 +0100

    Added the addition operation

--- src/main/java/ie/atu/week2sem1/microservices/Calculator.java
+++ src/main/java/ie/atu/week2sem1/microservices/Calculator.java
@@ -9,7 +9,11 @@ public class Calculator {
     @GetMapping("/calculate")
     public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation)
     {
-        return "Num1: " + num1 + " Num2: " + num2 + " Operation: " + operation;
+        int result = 0;
+        if (operation.equals("add")){
+            result = num1 + num2;
+        }
+        return String.valueOf(result);
     }
 
 }
